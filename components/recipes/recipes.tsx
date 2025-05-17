633122fbda0fa2ebee5a52e218a870cd8171002a
Jom Karlo Verzosa <jomkarlov@gmail.com>
2025-05-17 23:45:31 +0800

    feat: searchable recipes

--- components/recipes/recipes.tsx
+++ components/recipes/recipes.tsx
@@ -1,15 +1,32 @@
 import RecipeCard from "./recipeCard";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
+import { useState } from "react";
+import { Input } from "../ui/input";
 
 export default function Recipes() {
   const { results } = useSelector((state: RootState) => state.recipe);
+  const [search, setSearch] = useState<string>("");
+
+  const filteredResults = results.filter((recipe) =>
+    recipe.Name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="max-h-[50vh] flex flex-col gap-3 overflow-auto p-[1rem]">
-      {results.map((recipe, index) => (
+      <Input
+        type="text"
+        placeholder="Search recipes..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="mb-3 px-3 py-2 border rounded w-full"
+      />
+      {filteredResults.map((recipe, index) => (
         <RecipeCard key={index} recipe={recipe} />
       ))}
+      {filteredResults.length === 0 && (
+        <div className="text-gray-400 text-center">No recipes found.</div>
+      )}
     </div>
   );
 }
